137875379ff256353357b1d7e6d4f4c2cda1421b
Aren Chen <gucci313031@gmail.com>
2024-08-07 20:50:03 +0800

    improve Logger

--- src/Netcorext.Auth.Core/Models/Traffic.cs
+++ src/Netcorext.Auth.Core/Models/Traffic.cs
@@ -5,14 +5,14 @@ public class Traffic
     public DateTimeOffset TrafficDate { get; set; }
     public string Protocol { get; set; } = null!;
     public string Scheme { get; set; } = null!;
-    public string HttpMethod { get; set; } = null!;
+    public string Method { get; set; } = null!;
     public string Url { get; set; } = null!;
     public string Host { get; set; } = null!;
     public string Path { get; set; } = null!;
     public string? QueryString { get; set; }
     public string? Headers { get; set; }
     public string? ResponseHeaders { get; set; }
-    public string Status { get; set; } = null!;
+    public string StatusCode { get; set; } = null!;
     public TimeSpan Elapsed { get; set; }
     public string? DeviceId { get; set; }
     public string? Ip { get; set; }
